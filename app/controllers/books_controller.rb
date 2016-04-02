4cf59c43534aaec6263d2ad19e0967ebeed34ee2
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-02 01:36:15 +0000

    add action and fix bootstrap style error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,18 @@ class BooksController < ApplicationController
     def index
     end
 
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_param)
+    end
+
+    private
+
+        def book_param
+            params.require(:book).permit(:title,:description,:author)
+        end
+
 end
